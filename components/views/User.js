914b0cbd66fe3191e790c53727d1bd0e79f2ce5f
Joshua Deeke <jdeeke@outlook.com>
2020-06-28 17:55:04 -0500

    Update Capstone Project

--- components/views/User.js
+++ components/views/User.js
@@ -3,19 +3,19 @@ export default () => `
 <div class="song">
     <p class="songname">Great Are You Lord</p>
     <audio controls>
-        <source src="music/GreatAreYouLord.mp3" type="audio/mp3">
+        <source src="GreatAreYouLord.mp3" type="audio/mp3">
     </audio>
 </div>
 <div class="song">
     <p class="songname">My All in All</p>
     <audio controls>
-        <source src="music/MyAllinAll.mp3" type="audio/mp3">
+        <source src="MyAllinAll.mp3" type="audio/mp3">
     </audio>
 </div>
 <div class="song">
     <p class="songname">Tell Your Heart to Beat Again</p>
     <audio controls>
-        <source src="music/TellYourHearttoBeatAgain.mp3" type="audio/mp3">
+        <source src="TellYourHearttoBeatAgain.mp3" type="audio/mp3">
     </audio>
 </div>
 </div>
